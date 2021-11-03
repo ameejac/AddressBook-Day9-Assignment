554024b38bcecc03b549b8f145e698ef67af2415
Ameejac <ameejacherukuru123@gmail.com>
2021-11-03 20:19:51 +0530

    [created contacts in Address book]

---
+++ AddressBookDay9/AddressBookDay9/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookDay9
+{
+    class Contact
+    {
+        public string Firstname;
+        public string Lastname;
+        public string City;
+        public string State;
+        public int Phonenum;
+        public int Zip;
+        public string Email;
+        public  Contact(string firstname,string lastname,string address,string city,string state,int phonenum,int zip,String email)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Phonenum = phonenum;
+            this.State = state;
+            this.Zip = zip;
+            this.City = city;
+            this.Email = email;
+
+        }
+    }
+}
---
+++ AddressBookDay9/AddressBookDay9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AddressBookDay9
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //uc1 adding contact details in addressbook
+            //Addressbook addressBook = new Addressbook();
+            Console.WriteLine("enter first name:");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("enter last name:");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("enter address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("enter cityname:");
+            string city = Console.ReadLine();
+            Console.WriteLine("enter state name:");
+            string state = Console.ReadLine();
+            Console.WriteLine("enter phone number");
+            int phonenumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter zip number:");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter mail id");
+            string email = Console.ReadLine();
+
+         
+
+
+
+        }
+    }
+}
