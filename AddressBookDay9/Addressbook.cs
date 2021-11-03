65fc9b7eda1ef669d0fa8973e8dc1e36e26bf8ad
Ameejac <ameejacherukuru123@gmail.com>
2021-11-03 20:32:41 +0530

    [update contacts in Address book]

--- AddressBookDay9/Addressbook.cs
+++ AddressBookDay9/Addressbook.cs
@@ -11,24 +11,7 @@ namespace AddressBookDay9
         {
             contacts = new Contact[7];
         }
-        //uc2 add contacts in addressbook
-        public bool AddEntry(string firstname, string lastname, string address, string city, string state, int phonenum, int zip, String email)
-        {
-            Console.WriteLine("ContainsEntry:" + ContainsEntry(firstname));
-            if (!ContainsEntry(firstname))
-            {
-                Contact addcontact = new Contact(firstname, lastname, address, city, state, phonenum, zip, email);
-                Console.WriteLine("if issssssssssss");
-                contacts[0] = addcontact;
-                Console.WriteLine("firstNAme"+contacts[0].Firstname);
-
-            }
-            else {
-                Console.WriteLine($"({firstname}) already exists in Address Book!");
-
-            }
-            return false;
-        }
+       
         private bool ContainsEntry(string firstname)
         {
             return GetEntryIndex(firstname) != -1;
@@ -45,7 +28,33 @@ namespace AddressBookDay9
             }
             return -1;
         }
-       
+
+        //uc3 update contacts in addressbook
+        public bool updatecontact(string name)
+        {
+            Console.WriteLine("would you like to edit the cotact data in addressbook .type y or no:");
+            string updatedata = Console.ReadLine().ToLower();
+            Console.WriteLine("u can choose to change the  name or address or number ");
+                if (updatedata == "y")
+                {
+                    Console.WriteLine("please enter the updated contact data");
+                    string updatecontact = Console.ReadLine();
+
+                    int index = GetEntryIndex(name);
+                    Console.WriteLine("index:" + index);
+                    contacts[index].Firstname = updatecontact;
+                    Console.WriteLine("updatename:" + updatecontact);
+
+                }
+                else
+                {
+                    Console.WriteLine("data is not update:");
+                }
+            return false;
+        }
+
+
+    
 
 
 
